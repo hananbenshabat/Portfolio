7f8e8b89352d94a9719c826010fbe8eb3a094a4f
hananbenshabat <hanan.ben.shabat@s.afeka.ac.il>
2024-09-07 16:22:16 +0300

    fix: Deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from 'react-dom/client';
 import App from './app';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+const container = document.querySelector('#root');
+
+if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+}

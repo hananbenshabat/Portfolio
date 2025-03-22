2b9cb2819e4d76ef5813061938bbd4c2828e6bc1
hananbenshabat <hanan.ben.shabat@s.afeka.ac.il>
2025-03-22 10:43:56 +0200

    refactor: afeka new logo, timeline card mobile gradient

--- src/components/timeline-card.tsx
+++ src/components/timeline-card.tsx
@@ -14,49 +14,26 @@ const TimelineCard: React.FC<TimelineCardProps> = ({
 }) => {
     return (
         <div>
-            <div className="hidden lg:block">
-                <div className={`flex items-center justify-between w-full mb-8  ${styling}`}>
-                    <div className="order-1 w-5/12" />
-                    <div className="mr-2 ml-2 z-20 flex items-center order-1 h-8 bg-secondary-focus rounded-full shadow-xl">
-                        <h1 className="mr-2 ml-2 mx-auto text-lg font-semibold text-secondary-content">
-                            {start} - {end}
-                        </h1>
-                    </div>
-                    <motion.div
-                        transition={{ duration: 0.25 }}
-                        whileHover={{ scale: 1.1 }}
-                        className="order-1 w-5/12 shadow-2xl card lg:card-side text-primary-content from-base-300 via-primary bg-secondary bg-linear-to-br">
-                        <div className="card-body">
-                            <img src={logo} alt={company} className="w-20 max-h-20 max-w-20" />
-                            <span className="py-0 font-bold card-subtitle card-side">
-                                <div className="text-[20px]">{position}</div>
-                                <div className="text-[14px]">{company}</div>
-                            </span>
-                            <p className="card-side">{description}</p>
-                        </div>
-                    </motion.div>
+            <div className={`flex items-center justify-between w-full mb-8 ${styling}`}>
+                <div className="hidden lg:flex order-1 w-5/12" />
+                <div className="hidden lg:flex mx-2 z-20 items-center order-1 h-8 bg-secondary-focus rounded-full shadow-xl">
+                    <h1 className="px-2 text-lg font-semibold text-secondary-content">
+                        {start} - {end}
+                    </h1>
                 </div>
-            </div>
-
-            <div className={`lg:hidden items-center justify-between w-full mb-8  ${styling}`}>
-                <div className="" />
                 <motion.div
                     transition={{ duration: 0.25 }}
                     whileHover={{ scale: 1.1 }}
-                    className={
-                        num % 2 === 0
-                            ? 'shadow-2xl card lg:card-side bg-primary-focus text-primary-content'
-                            : 'shadow-2xl card lg:card-side bg-secondary text-secondary-content'
-                    }>
+                    className="order-1 lg:w-5/12 shadow-2xl card lg:card-side text-primary-content from-info via-primary bg-secondary bg-linear-to-br">
                     <div className="card-body">
-                        <span className="py-0 font-bold card-subtitle card-side">
-                            <h1 className="mx-auto text-lg font-semibold text-secondary-content">
-                                {start} - {end}
-                            </h1>
-                            <img src={logo} alt={company} className="w-20 max-h-20 max-w-20" />
-                            <div className="text-[20px]">{position}</div>
-                            <div className="text-[14px]">{company}</div>
-                        </span>
+                        <h1 className="lg:hidden text-lg font-semibold text-secondary-content">
+                            {start} - {end}
+                        </h1>
+                        <img src={logo} alt={company} className="w-20 max-h-20 max-w-20" />
+                        <div className="card-subtitle card-side flex flex-col font-bold">
+                            <span className="text-[20px]">{position}</span>
+                            <span className="text-[14px]">{company}</span>
+                        </div>
                         <p className="card-side">{description}</p>
                     </div>
                 </motion.div>
